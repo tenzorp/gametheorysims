2a4c92e6efcd9028b5a4d9b2c55ee73b4155001a
Tenzin Dorjee <tdorjee@middlebury.edu>
2019-06-27 17:38:33 -0400

    Finished company game, need to test random repairing

--- games/company/models.py
+++ games/company/models.py
@@ -2,28 +2,54 @@ from otree.api import (
     models, widgets, BaseConstants, BaseSubsession, BaseGroup, BasePlayer,
     Currency as c, currency_range
 )
+import random
 
-
-author = 'Your name here'
-
-doc = """
-Your app description
+"""
+Sim for 'Acquiring a Company' game
 """
 
 
 class Constants(BaseConstants):
     name_in_url = 'company'
-    players_per_group = None
-    num_rounds = 1
+    players_per_group = 2
+    num_rounds = 2
+
+    instructions_template = 'company/instructions.html'
 
 
 class Subsession(BaseSubsession):
-    pass
+    def creating_session(self):
+        self.group_randomly()
+        for group in self.get_groups():
+            group.value = c(random.uniform(0, 100))
 
 
 class Group(BaseGroup):
-    pass
+    value = models.CurrencyField(min=0)
 
 
 class Player(BasePlayer):
-    pass
+    price = models.CurrencyField(min=0)
+
+    def role(self):
+        if self.round_number == 1:
+            if self.id_in_group == 1:
+                return 'Buyer'
+            else:
+                return 'Seller'
+        else:
+            if self.id_in_group == 2:
+                return 'Buyer'
+            else:
+                return 'Seller'
+
+    def set_payoff(self):
+        buyer = self.group.get_player_by_role('Buyer')
+        seller = self.group.get_player_by_role('Seller')
+        if buyer.price >= seller.price:
+            buyer.payoff = 1.5 * self.group.value
+            seller.payoff = buyer.price
+        else:
+            buyer.payoff = 0
+            seller.payoff = self.group.value
+
--- games/company/pages.py
+++ games/company/pages.py
@@ -3,22 +3,44 @@ from ._builtin import Page, WaitPage
 from .models import Constants
 
 
-class MyPage(Page):
-    pass
+class Introduction(Page):
+
+    def is_displayed(self):
+        return self.round_number == 1
+
+
+class Main(Page):
+    form_model = 'player'
+    form_fields = ['price']
+
+    def vars_for_template(self):
+        if self.player.role() == 'Seller':
+            return {
+                'value': self.group.value
+            }
 
 
 class ResultsWaitPage(WaitPage):
 
     def after_all_players_arrive(self):
-        pass
+        for p in self.group.get_players():
+            p.set_payoff()
 
 
 class Results(Page):
-    pass
+
+    def vars_for_template(self):
+        opponent = self.player.get_others_in_group()[0]
+        return {
+            'player_payoff': self.player.payoff,
+            'opponent': opponent,
+            'value': self.group.value
+        }
 
 
 page_sequence = [
-    MyPage,
+    Introduction,
+    Main,
     ResultsWaitPage,
     Results
 ]
