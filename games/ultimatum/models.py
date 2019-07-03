7e97fea13a0077b6949bd67a01fe5df41287894f
Tenzin Dorjee <tdorjee@middlebury.edu>
2019-07-03 16:00:47 -0400

    half of ultimatum game

--- games/ultimatum/models.py
+++ games/ultimatum/models.py
@@ -1,20 +1,21 @@
 from otree.api import (
     models, widgets, BaseConstants, BaseSubsession, BaseGroup, BasePlayer,
-    Currency as c, currency_range
+    Currency as c
 )
+import random
 
-
-author = 'Your name here'
-
-doc = """
-Your app description
+"""
+Sim for Ultimatum/Two-Stage Bargaining Game
 """
 
 
 class Constants(BaseConstants):
     name_in_url = 'ultimatum'
-    players_per_group = None
+    players_per_group = 2
     num_rounds = 1
+
+    instructions_template = 'ultimatum/instructions.html'
+    role = random.choice([1, 2])
 
 
 class Subsession(BaseSubsession):
@@ -26,4 +27,12 @@ class Group(BaseGroup):
 
 
 class Player(BasePlayer):
-    pass
+
+    def role(self):
+        if self.id_in_group == Constants.role:
+            return 'proposer'
+        else:
+            return 'responder'
+
+    def other_player(self):
+        return self.get_others_in_group()[0]
